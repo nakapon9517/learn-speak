bdb1f92e7bcfcfb91a8cb2a273daa393b7840306
nakapon9517 <maksminakapon@gmail.com>
2020-05-25 23:03:39 +0900

    change voice

--- src/views/components/voice.js
+++ src/views/components/voice.js
@@ -8,7 +8,7 @@ export const speak = (text) => {
 
   let options = new SpeechSynthesisUtterance(text);
   options.lang = "ja-JP";
-  options.voice = voices[7];
+  // options.voice = voices[7];
   options.rate = 1.5;
   options.pitch = 0.3;
   window.speechSynthesis.speak(options);
