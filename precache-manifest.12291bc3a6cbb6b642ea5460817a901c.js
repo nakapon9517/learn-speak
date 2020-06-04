7fafab4a223c07f41c470e5c66f4caf0069d57dd
nakapon9517 <maksminakapon@gmail.com>
2020-06-04 23:59:12 +0900

    Updates

---
+++ precache-manifest.12291bc3a6cbb6b642ea5460817a901c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c25adddbee12a44cde05e550db59798",
+    "url": "/learn-speak/index.html"
+  },
+  {
+    "revision": "ec3528f03ef37f42b199",
+    "url": "/learn-speak/static/css/2.98a57b59.chunk.css"
+  },
+  {
+    "revision": "ec7b824a55d7bf984691",
+    "url": "/learn-speak/static/css/main.9900741b.chunk.css"
+  },
+  {
+    "revision": "ec3528f03ef37f42b199",
+    "url": "/learn-speak/static/js/2.fae76d6f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/learn-speak/static/js/2.fae76d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7b824a55d7bf984691",
+    "url": "/learn-speak/static/js/main.666d950c.chunk.js"
+  },
+  {
+    "revision": "d74dc0f3e2dc43a33443",
+    "url": "/learn-speak/static/js/runtime-main.4b15a5fe.js"
+  }
+]);
